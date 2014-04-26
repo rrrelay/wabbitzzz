ef065f51d0ae37b3448f5244dca9c2db6acd77cf
Mike Chevett <mchevett@gmail.com>
2014-04-26 16:25:39 -0400

    check env vars and log errors

--- exchange.js
+++ exchange.js
@@ -1,6 +1,7 @@
 var q = require('q');
 var amqp = require('amqp');
 var _ = require('lodash');
+var prettyError = require('prettify-error');
 
 var DEFAULTS = {
 	type:'fanout',
@@ -11,11 +12,19 @@ var DEFAULTS = {
 
 function _getExchange(params){
 	var d = q.defer();
-	var connection = amqp.createConnection({ host: '127.0.0.1' });
+	var connection = amqp.createConnection({ url: process.env.WABBITZZZ_URL || 'amqp://localhost' });
 
 	var name = params.name;
 	delete params.name;
 	params = _.extend({}, DEFAULTS, params);
+
+	connection.addListener('error', function(err){
+		if (global.logger){
+			global.logger.error(prettyError(err) || err);
+		} else {
+			console.error(prettyError(err) || err);
+		}
+	});
 
 	connection.addListener('ready', function(){
 		var exchange = connection.exchange(name, params);
