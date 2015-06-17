7efac24e2bdd9df28e94ba879ceff102127c1b07
Mike Chevett <mchevett@gmail.com>
2015-06-16 23:19:36 -0400

    oh right

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var Exchange = require('./exchange');
 var Queue = require('./queue');
+var request = require('./request');
+var response = require('./response');
 
 module.exports = {
 	Exchange: Exchange,
-	Queue: Queue
+	Queue: Queue,
+	request: request,
+	response: response,
+
 };
