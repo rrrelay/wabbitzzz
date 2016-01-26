dd03bc9979292933313bc0fe8ca45fd883022a81
Mike Chevett <mchevett@gmail.com>
2016-01-26 14:13:54 -0500

    expose get stuff

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ var Queue = require('./queue');
 var request = require('./request');
 var response = require('./response');
 
+var getResponse = require('./get/response');
+var getRequest = require('./get/request');
+
 module.exports = {
 	Exchange: Exchange,
 	Queue: Queue,
 	request: request,
 	response: response,
-
+	get: {
+		request: getRequest,
+		response: getResponse,
+	},
 };
