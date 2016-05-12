2d2a9646696fad68cb57d0b850082f1c3bc78a9f
Mike Chevett <mchevett@gmail.com>
2016-05-12 18:52:19 -0400

    couple more tests

--- exchange.spec.js
+++ exchange.spec.js
@@ -73,6 +73,132 @@ describe('exchange', function(){
 
 	});
 
+	it('should be able to publish a delayed message to a topic exchange', function(done){
+		this.timeout(10000);
+
+		var message = ezuuid(),
+			exchangeName = ezuuid();
+
+		var beginTicks;
+
+		var exchange = new Exchange({
+				autoDelete: true,
+				type: 'topic',
+				name: exchangeName
+			});
+
+		exchange.on('ready', function(){
+			var queue = new Queue({
+				autoDelete: true,
+				exclusive: true,
+				exchangeNames: [exchangeName],
+				key: '111',
+				ready: function(){
+					beginTicks = Date.now();
+					exchange.delayedPublish({message: message}, {delay:3100, key: '111' });
+				}
+			});
+
+			queue(function(msg, ack){
+				if (msg.message !== message) return done('got a message I shouldnt have');
+
+				var timeDiff = Date.now() - beginTicks;
+
+				if (timeDiff<=3000){ // TODO: somehow a tiny bit off, it's cool with me atm
+					return done(new Error('too fast'));
+				}
+
+				ack();
+				done();
+			});
+		});
+
+	});
+
+	it('should be able to publish a delayed message to a direct exchange', function(done){
+		this.timeout(10000);
+
+		var message = ezuuid(),
+			exchangeName = ezuuid();
+
+		var beginTicks;
+
+		var exchange = new Exchange({
+				autoDelete: true,
+				type: 'direct',
+				name: exchangeName
+			});
+
+		exchange.on('ready', function(){
+			var queue = new Queue({
+				autoDelete: true,
+				exclusive: true,
+				key: '111',
+				exchangeNames: [exchangeName],
+				ready: function(){
+					beginTicks = Date.now();
+					exchange.delayedPublish({message: message}, {delay:3100, key: '111' });
+				}
+			});
+
+			queue(function(msg, ack){
+				if (msg.message !== message) return done('got a message I shouldnt have');
+
+				var timeDiff = Date.now() - beginTicks;
+
+				if (timeDiff<=3000){ // TODO: somehow a tiny bit off, it's cool with me atm
+					return done(new Error('too fast'));
+				}
+
+				ack();
+				done();
+			});
+		});
+
+	});
+
+	it('should be able to publish a delayed message to an x-lvc exchange', function(done){
+		this.timeout(10000);
+
+		var message = ezuuid(),
+			exchangeName = ezuuid();
+
+		var beginTicks;
+
+		var exchange = new Exchange({
+				autoDelete: true,
+				type: 'x-lvc',
+				name: exchangeName
+			});
+
+		exchange.on('ready', function(){
+			var queue = new Queue({
+				autoDelete: true,
+				exclusive: true,
+				key: 'fun',
+				exchangeNames: [exchangeName],
+				ready: function(){
+					beginTicks = Date.now();
+					exchange.delayedPublish({message: message}, {delay:3100, key: 'fun' });
+				}
+			});
+
+			queue(function(msg, ack){
+				if (msg.message !== message) return done('got a message I shouldnt have');
+
+				var timeDiff = Date.now() - beginTicks;
+
+				if (timeDiff<=3000){ // TODO: somehow a tiny bit off, it's cool with me atm
+					return done(new Error('too fast'));
+				}
+
+				ack();
+				done();
+			});
+		});
+
+	});
+
 	it('should be able to receive a publish confirm', function(done){
 		this.timeout(5000);
 
