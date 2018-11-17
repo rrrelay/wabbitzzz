1a6df80598ce0fe98b6e3180b2edb45f4954e9a6
Mike Chevett <mchevett@gmail.com>
2018-11-17 14:34:24 -0500

    more process.exit if anything goes wrong with the connection

--- get-connection.js
+++ get-connection.js
@@ -2,26 +2,52 @@ var CONN_STRING = process.env.WABBITZZZ_URL || 'amqp://localhost';
 var amqplib = require('amqplib');
 var Promise = require('bluebird');
 
+function _log(...args) {
+	if (global.logger && global.logger.warn) {
+		global.logger.warn.apply(global.logger, args);
+	} else {
+		console.warn.apply(console, args);
+	}
+}
+
 function _getConnection(){
 	return Promise.resolve(amqplib.connect(CONN_STRING))
 		.then(function(conn) {
 			var closed = false;
 			function close(){
-				if (closed) return;
+				if (closed){
+					_log('close already ran');
+					return;
+				}
+				_log('running close');
 				closed = true;
 				conn.close();
 			}
 
 			process.once('SIGINT', close);
 			conn.on('close', closeData => {
-				console.log('WABBITZZZ CONNECTION CLOSED', closeData);
-				process.exit(1);
+				_log('WABBITZZZ CONNECTION CLOSED', closeData);
+				setTimeout(function() {
+					_log(`connection closed EXITING NOW.`);
+					process.exit(1);
+				}, 5000);
+			});
+
+			conn.on('error', err => {
+				_log('WABBITZZZ CONNECTION ERRROR', err);
 			});
 
 			return conn;
 		})
+		.timeout(30000)
 		.catch(function(err){
-			console.error('wabbitzzz connection error: ', err);
+			_log(`unable to get connect: ${err.message}`);
+
+			setTimeout(function() {
+				_log(`unable to get connect EXITING NOW: ${err.message}`);
+				process.exit(1);
+			}, 5000);
+
 			throw err;
 		});
 }
