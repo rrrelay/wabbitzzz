bed0d5929cf6cf759e107abe44a2740620c4e1ce
Andrew Craft <drewtcraft@gmail.com>
2020-03-05 13:07:28 -0500

    kill some stuff

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ var Exchange = require('./exchange');
 var Queue = require('./queue');
 var request = require('./request');
 var response = require('./response');
-
-var getResponse = require('./get/response');
-var getRequest = require('./get/request');
-
-var bulkDequeue = require('./bulk-dequeue');
 var rpc = require('./rpc');
 
 module.exports = {
@@ -14,10 +9,5 @@ module.exports = {
 	Queue: Queue,
 	request: request,
 	response: response,
-	get: {
-		request: getRequest,
-		response: getResponse,
-	},
-	bulkDequeue: bulkDequeue,
 	rpc: rpc,
 };
