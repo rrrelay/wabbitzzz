0186cadaaaa53c0b4e51a0e909a2960997c3bde0
Andrew Craft <drewtcraft@gmail.com>
2020-07-20 11:29:01 -0400

    improve comment

--- queue.js
+++ queue.js
@@ -220,7 +220,7 @@ function Queue(params){
 							const { routingKey } = msg.fields;
 							// there are potentially many matches, but we just use the first one. meh.
 							const matchedBinding = _.find(labeledBindings, b => {
-								// it is possible to apply the wrong label to an exchange if
+								// it is possible to apply the wrong label to a message if
 								// we do not specify the exchange
 								const isRightExchange = b.name === myMessage._exchange;
 								return b.isMatch && b.isMatch(routingKey) && isRightExchange;
